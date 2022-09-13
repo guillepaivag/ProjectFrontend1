75172a770d0642e84c0177a9f8727cdd92943a62
alcabvaldo <jandronico@hotmail.com>
2022-09-13 18:01:51 -0400

    feat añade funcion a ficha-clinica

---
+++ src/app/model/ficha-clinica.model.spec.ts
@@ -0,0 +1,7 @@
+import { FichaClinica } from './ficha-clinica.model';
+
+describe('FichaClinica', () => {
+  it('should create an instance', () => {
+    expect(new FichaClinica()).toBeTruthy();
+  });
+});

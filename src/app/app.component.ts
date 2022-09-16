304393379bcafb28744500409c5d98a7e418db70
jg2kpy <jlgutierrez2000@hotmail.com>
2022-09-16 11:28:30 -0400

    Se cambio el title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-bootstrap-examples';
+  constructor(private titleService: Title) {
+    titleService.setTitle("Proyecto Frontend Grupo X");
+  }
 }
